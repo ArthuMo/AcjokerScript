a6668a6f5919d487d161096016f18764807f006c
acjoker8818 <112934795+acjoker8818@users.noreply.github.com>
2022-10-01 07:18:56 -0500

    Add files via upload

--- AcjokerScript.lua
+++ AcjokerScript.lua
@@ -3988,7 +3988,7 @@ Dlcp = {
 
 players.dispatch_on_join()
 
-local localVer = 1.5 -- all credits for the updater go to Prisuhm#7717 Thank You
+local localVer = 1.6 -- all credits for the updater go to Prisuhm#7717 Thank You
 
  -------------------
  AClang.action(setroot, 'Version Number', {}, tostring(localVer), function ()
